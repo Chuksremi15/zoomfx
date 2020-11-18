b3121ec36719c08aa819d08a8858b1210a7b5ff1
Chuksremi15 <59108605+Chuksremi15@users.noreply.github.com>
2020-11-18 08:44:15 +0100

    make it better

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const colors = require('colors');
 const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Route files
 const users = require('./routes/users');
@@ -41,6 +42,15 @@ app.use('/api/v1/notifications', notifications);
 
 // Error handler middleware
 app.use(errorHandler);
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
