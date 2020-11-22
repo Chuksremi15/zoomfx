9c86d697d553551d639dd257e17b643ade26daad
Chuksremi15 <59108605+Chuksremi15@users.noreply.github.com>
2020-11-22 12:01:20 +0100

    work

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const colors = require('colors');
 
 const errorHandler = require('./middleware/error');
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,5 +1,4 @@
 const nodemailer = require('nodemailer');
-const {options} = require('../routes/auth');
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
