71a6122e9fe2785da87cc3f92232d284563d090b
Chuksremi15 <59108605+Chuksremi15@users.noreply.github.com>
2020-11-22 10:36:11 +0100

    made some changes

--- server.js
+++ server.js
@@ -47,10 +47,6 @@ app.use(errorHandler);
 if (process.env.NODE_ENV === 'production') {
   // set static folder
   app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.send(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
 }
 
 const PORT = process.env.PORT || 5000;
