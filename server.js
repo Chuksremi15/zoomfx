e51eaa883726725f2cb17fcb42b5bfc12906d693
Chuksremi15 <59108605+Chuksremi15@users.noreply.github.com>
2020-11-23 09:24:32 +0100

    changes to

--- server.js
+++ server.js
@@ -45,11 +45,10 @@ app.use(errorHandler);
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  // set static folder
-  app.use(express.static('client/build'));
-
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
   app.get('*', (req, res) => {
-    res.send(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join((__dirname = 'client/build/index.html')));
   });
 }
 
